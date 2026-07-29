ad7368cf7899f2bd2468fc9b611bd7e49190569f
amitee <amitkumar023410@gmail.com>
2026-07-29 21:00:02 +0530

    Add app entry point and dark theme styles for map UI

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StressMap from './components/StressMap'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <StressMap />
+  </React.StrictMode>
+)
